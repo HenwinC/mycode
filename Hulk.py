1aba47abefca1db32a0f3dacb6181f72166a62f0
Henwin <h.cuffy@hotmail.com>
2022-05-18 19:47:57 +0000

    learning about dictionaries

---
+++ Hulk.py
@@ -0,0 +1,18 @@
+# Hulk={
+# "Real Name":"Robert Bruce Banner",
+# "Creators":"Stan Lee & Jack Kirby",
+# "Powers":["Superhuman Strength, Superhuman Durability, Superhuman Stamina, Superhuman Speed, Accelerated Healing Factor"],
+# "Paraphernalia":"None",
+# "Personality":["Angry, hot-tempered, powerful, strong, tough, loud, comical, brave, good-hearted"]
+# }
+# print(Hulk)
+
+Deadpool={'Real Name':'Wade Winston Wilson',
+'Alias':'Deadpool',
+'First Appearance':'New Mutants (Vol. 1) #98 (February, 1991)',
+'Creators':['Fabian Nicieza',' Rob Liefeld'],
+'Base of Operations':'Nomadic'}
+
+print(Deadpool["Real Name"])
+
+print(f"Deadpool aka {Deadpool['Real Name']}, aka {Deadpool['Alias'] }, was created by {Deadpool['Creators']}, he made his first apperence in {Deadpool['First Appearance']}, His base of operation is {Deadpool['Base of Operations']}")
