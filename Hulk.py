0a59bdb2d71fdb533593ac683527ca702e6339e5
Henwin <h.cuffy@hotmail.com>
2022-05-19 16:45:49 +0000

    working on if, else logic

--- Hulk.py
+++ Hulk.py
@@ -1,18 +1,23 @@
-# Hulk={
-# "Real Name":"Robert Bruce Banner",
-# "Creators":"Stan Lee & Jack Kirby",
-# "Powers":["Superhuman Strength, Superhuman Durability, Superhuman Stamina, Superhuman Speed, Accelerated Healing Factor"],
-# "Paraphernalia":"None",
-# "Personality":["Angry, hot-tempered, powerful, strong, tough, loud, comical, brave, good-hearted"]
-# }
+Hulk={
+"Real Name":"Robert Bruce Banner",
+"Creators":"Stan Lee & Jack Kirby",
+"Powers":["Superhuman Strength, Superhuman Durability, Superhuman Stamina, Superhuman Speed, Accelerated Healing Factor"],
+"Paraphernalia":"None",
+"Personality":["Angry, hot-tempered, powerful, strong, tough, loud, comical, brave, good-hearted"]
+}
 # print(Hulk)
 
-Deadpool={'Real Name':'Wade Winston Wilson',
-'Alias':'Deadpool',
-'First Appearance':'New Mutants (Vol. 1) #98 (February, 1991)',
-'Creators':['Fabian Nicieza',' Rob Liefeld'],
-'Base of Operations':'Nomadic'}
+Hulk["fav-food"] = "Taco"
+print(Hulk.keys())
+choice = input("Choose your key: ")
+print(Hulk[choice])
 
-print(Deadpool["Real Name"])
+# Deadpool={'Real Name':'Wade Winston Wilson',
+# 'Alias':'Deadpool',
+# 'First Appearance':'New Mutants (Vol. 1) #98 (February, 1991)',
+# 'Creators':['Fabian Nicieza',' Rob Liefeld'],
+# 'Base of Operations':'Nomadic'}
 
-print(f"Deadpool aka {Deadpool['Real Name']}, aka {Deadpool['Alias'] }, was created by {Deadpool['Creators']}, he made his first apperence in {Deadpool['First Appearance']}, His base of operation is {Deadpool['Base of Operations']}")
+# print(Deadpool["Real Name"])
+
+# print(f"Deadpool aka {Deadpool['Real Name']}, aka {Deadpool['Alias'] }, was created by {Deadpool['Creators']}, he made his first apperence in {Deadpool['First Appearance']}, His base of operation is {Deadpool['Base of Operations']}")
