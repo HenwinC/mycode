c1c7c549a31a7bea3b356e8377cac3e4c46a00fe
Henwin <h.cuffy@hotmail.com>
2022-05-19 17:58:38 +0000

    While looping

---
+++ if-logic-challenge/WTOD.py
@@ -0,0 +1,12 @@
+yesNoLogic = "Y = Yes and N = No"
+
+name = input("Please enter your name")
+
+print (f"Hello {name} We hear your looking for A new puppy to bring home. We are here to help! Lets get sharted.")
+
+questionOne = input(f"Will you new dog have a job?{yesNoLogic}")
+if questionOne.lower() == y:
+    questionTwo = input("Will it be a Guard Dog, hearding dog, or Service animal  ")
+
+
+
