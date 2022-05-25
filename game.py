eaebcb02131da9fda6aff0acb3252777fe6c4fa0
Henwin <h.cuffy@hotmail.com>
2022-05-25 21:32:08 +0000

    My first Flask app

--- game.py
+++ game.py
@@ -3,8 +3,7 @@ import colorama
 from colorama import Fore
 import random
 
-thieve_health = 100
-player_health = 100
+
 
 # Replace RPG starter project with this code when new instructions are live
 
@@ -15,7 +14,7 @@ Survial Game
 ========
 Commands:
   go [up, down, left, right]
-  get [item]
+  grab [item]
 ''')
 
 def showStatus():
@@ -25,8 +24,11 @@ def showStatus():
   #print the current inventory
   print('Inventory : ' + str(inventory))
   #print an item if there is one
+  print(rooms[currentRoom]["description"])
   if "item" in rooms[currentRoom]:
     print('You see a ' + rooms[currentRoom]['item'])
+
+  print(rooms[currentRoom]["description"])
   print("---------------------------")
 
 #an inventory, which is initially empty
@@ -38,6 +40,7 @@ rooms = {
             'Bedroom' : { 
                   'down' : 'Hallway',
                   'item' : 'butter knife',
+            'description': 'A typical bedroom with no major weapons. Thankfully you made that PB and J sandwhich last night.',
                 },
 
             'Hallway' : { 
@@ -69,28 +72,29 @@ rooms = {
 currentRoom = 'Bedroom'
 
 def combat_Mode():
+  thieve_health = 100
+  player_health = 100
   while currentRoom == 'Kitchen':
     if inventory == False:
       print(' You ran into the thieve empty handed! Comonnnnn Dawg... GAME OVER!')
       break
-    
-    print(f'Thieve Health {thieve_health} Your Health {player_health}')
     player = input(f'choose you weapon {inventory}: ')
     if player == "butter knife":
-      print(thieve_health)
       thieve_health = thieve_health - 7
-      print (f'Shank shank Thieve health -7 {thieve_health}')
+      print (f'Shank shank Thieve health  {thieve_health}')
     if player == "grandmas sandal":
       thieve_health = thieve_health -20
       print(f'The Power of Granny runs strong in this one. Thieve health {thieve_health}')
-    if player == 'shotgun':
-      thieve_health = thieve_health - 50
-      print(f'No ammo but a butt stroke works just fine with a shotgun {thieve_health}')
-    if player == 'Shotgun' and 'shotgun shells' in inventory:
+    if player == 'shotgun' and 'shotgun shells' in inventory:
       thieve_health = thieve_health - 100
       print(f'Bang Bang. That Thieve is goooonnneeeeeee!')
-    if thieve_health == 0:
-      print(f'Nice Job. That theive is goooooooonnneeee')
+    if player == 'shotgun' and 'shotgun shells' not in inventory:
+      thieve_health = thieve_health - 50
+      print(f'No ammo but a butt stroke works just fine with a shotgun {thieve_health}')
+    if thieve_health <= 0:
+      print(f'You Defended your house!')
+      False
+      break
     weapon = random.randint(1, 3)
     if weapon == 1:
       player_health = player_health - 20
@@ -101,38 +105,15 @@ def combat_Mode():
     if weapon == 3:
       player_health = player_health - 3
       print(f'Thieve hits you with a open hand smack....Disrespecful {player_health}')
-    if player_health == 0:
-      print(f'RIP. You Lose')
+    if player_health <= 0:
+      print(f'RIP. You Lose, lets restart')
+      currentRoom == 'Bedroom'
+      False
       break
-    if thieve_health == 0:
+    if thieve_health <= 0:
       print(f'You Defended your house! That thieve is goooooonnnnneeeeee')
+      False
       break
-    
-
-  
-
-# def color_screen():
-#   if currentRoom == "Bedroom":
-#     print(Fore.BLUE + ' ')
-#     Fore.BLUE
-#     Fore.RESET
-
-#   if currentRoom == "Hallway":
-#     print(Fore.GREEN + ' yurt')
-    
-#     Fore.GREEN
-
-#   if rooms[currentRoom] == 'Living Room':
-#     print(Fore.GREEN + '')
-#     Fore.MAGENTA
-#   if rooms[currentRoom] == 'Kitchen':
-#     print(Fore.GREEN + '')
-#     Fore.RED
-#   if currentRoom == 'Garage':
-#     print(Fore.GREEN + '')
-#     Fore.LIGHTYELLOW_EX
-#   if currentRoom == 'Basement':
-#     print(Fore.LIGHTGREEN_EX  + '')
 
 # color_screen()
 showInstructions()
@@ -141,6 +122,10 @@ while True:
 
   showStatus()
 
+  if currentRoom == 'Kitchen':
+    print('COMBAT MODE')
+    print(f'Thieve Health 100: Your Health 100')
+    combat_Mode()
 # 
   #get the player's next 'move'
   #.split() breaks it up into an list array
@@ -165,7 +150,7 @@ while True:
         print('You can\'t go that way!')
 
   #if they type 'get' first
-  if move[0] == 'get' :
+  if move[0] == 'grab' :
     #if the room contains an item, and the item is the one they want to get
     if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
       #add the item to their inventory
@@ -177,45 +162,4 @@ while True:
     #otherwise, if the item isn't there to get
     else:
       #tell them they can't get it
-      print('Can\'t get ' + move[1] + '!')
-
-if rooms[currentRoom] == 'Kitchen':
-  print('COMBAT MODE')
-  print(f'Thieve Health {thieve_health} Your Health {player_health}')
-  combat_Mode()
-    # player = input(f'choose you weapon {inventory}: ')
-    # if player == "butter knife":
-    #   print(thieve_health)
-    #   thieve_health = thieve_health - 7
-    #   print (f'Shank shank Thieve health -7 {thieve_health}')
-    # if player == "grandmas sandal":
-    #   thieve_health = thieve_health -20
-    #   print(f'The Power of Granny runs strong in this one. Thieve health {thieve_health}')
-    # if player == 'shotgun':
-    #   thieve_health = thieve_health - 50
-    #   print(f'No ammo but a butt stroke works just fine with a shotgun {thieve_health}')
-    # if player == 'Shotgun' and 'shotgun shells' in inventory:
-    #   thieve_health = thieve_health - 100
-    #   print(f'Bang Bang. That Thieve is goooonnneeeeeee!')
-    # if thieve_health == 0:
-    #   print(f'Nice Job. That theive is goooooooonnneeee')
-    # weapon = random.randint(1, 3)
-    # if weapon == 1:
-    #   player_health = player_health - 20
-    #   print(f'Thieve sticks you with a knife attack {player_health}')
-    # if weapon == 2:
-    #   player_health = player_health - 50
-    #   print(f'Bang Bang. Thieve shots you {player_health}')
-    # if weapon == 3:
-    #   player_health = player_health - 3
-    #   print(f'Thieve hits you with a open hand smack....Disrespecful {player_health}')
-    # if player_health == 0:
-    #   print(f'RIP. You Lose')
-    #   break
-    # if thieve_health == 0:
-    #   print(f'You Defended your house! That thieve is goooooonnnnneeeeee')
-    #   break
-  ## Define how a player can win
-  ## If a player enters a room with a monster
-elif rooms[currentRoom] == 'Kitchen' and inventory == False:
-  print(' You ran into the thieve empty handed! Comonnnnn Dawg... GAME OVER!')
+      print('Can\'t grab ' + move[1] + '!')
