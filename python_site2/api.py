1183bf11e9b6ee1ed9198e0035060d5ae7b5192f
Henwin <h.cuffy@hotmail.com>
2022-05-27 20:23:40 +0000

    Flask-Project-Complete

--- python_site2/api.py
+++ python_site2/api.py
@@ -1,7 +1,11 @@
 import requests
 
-url = ""
-data = requests.get(url).json()
+url = "http://10.13.46.74:2225/jsondata"
+data = requests.get(url)
+
+print(data)
+
+data= data.json()
 
 for x in data:
-    print(f"{x['name']} is a great weapon choice. Its weapon class is {x['class']}.")
+    print(f"{x['name']} is considered the greatest player of all time. His favorite weapon is the {x['class']} and his playstle is {x['player_type']}")
--- python_site2/app.py
+++ python_site2/app.py
@@ -3,10 +3,13 @@ from flask import redirect
 from flask import url_for
 from flask import request
 from flask import render_template
-
+import json
 app = Flask(__name__)
+
 data = [{
-   'name': 'Bobby'
+   'name': 'Bobby',
+   'class': 'pkm',
+   'player_type': 'Run&Gun'
 }]
 
 
@@ -18,16 +21,17 @@ def congrats():
 def start():
    return render_template("app.html")
 
-@app.route("/question", methods = ["POST"])
+@app.route("/question", methods = ["POST","GET"])
 def question():
-   if request.form.get("guess") and request.form.get.lower("guess") == "warzone": #not sure if i can place lower() there
+   if request.form.get("guess") and request.form.get("guess").lower() == "warzone": #not sure if i can place lower() there
       return redirect("/congrats")
    else:
       return redirect("/")
 
 @app.route("/jsondata")
 def jsondata():
-   return jsonify(data)
+   return json.dumps(data)
+   
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=2224) # runs the application
-   # app.run(host="0.0.0.0", port=2224, debug=True) # DEBUG MODE
+   app.run(host="0.0.0.0", port=2225) # runs the application
+   # app.run(host="0.0.0.0", port=2225, debug=True) # DEBUG MODE
